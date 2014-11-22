ada7b1befdf6ff0d2b1efee8c29a174f6fe028fe
FrancoAguilera <flaalf@gmail.com>
2014-11-22 14:29:23 -0300

    Add the skeleton css files to improve UI and create a simple landing page with a minimal description

---
+++ grunt/Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		concat: {
+			options: {
+				separator: '/**/'
+			},
+			dist: {
+				src: ['../public/css/cssDev/base.css', '../public/css/cssDev/skeleton.css', '../public/css/cssDev/layout.css'],
+				dest: '../public/css/self.css'
+			}
+		},
+
+		cssmin: {
+		  combine: {
+		    files: {
+		      '../public/css/self.min.css': ['../public/css/self.css']
+		    }
+		  }
+		}
+
+	});
+
+
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+
+	grunt.registerTask('default', ['concat', 'cssmin']);
+
+};
